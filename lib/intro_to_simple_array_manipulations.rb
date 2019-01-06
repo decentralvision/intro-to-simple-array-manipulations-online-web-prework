a71adec0eedf494674caddebff78ea0422cdba64
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 00:28:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,14 @@ def using_pop(array)
   array.pop()
 end
 
-def pop_with_args(array, num)
-  array.pop(num)
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
 end
